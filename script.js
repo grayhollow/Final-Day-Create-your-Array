c488fe1415911b084e825da57dd2390031c68086
grayhollow <grayhollow7@gmail.com>
2025-08-24 20:02:18 +0300

    Correctly applied code learned for arrays
    Arrays listed and function properly. User can add or remove entries, with each input also updating the list.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Create a empty array to store the names here 
-
+let un = []
 
 // create a function that can add names to the array and print it in the list here 
+function addun(){
+let inputVal = document.getElementById("name").value;
+un.push(inputVal);
+console.log(un);
 
+[inputVal].forEach(usernames => { 
+    document.getElementById("namesList").innerHTML += "<li>" + usernames + "</li>";
+});
+}
 
 
 
 // create a function that can remove the last name from the array and print it in the list here 
 
+function remun(){
+un.pop(un)
+console.log(un)
 
+let remover = document.getElementById("namesList");
+remover.removeChild(remover.lastChild);
+}
 
 
 
